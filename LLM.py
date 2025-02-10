7d06ed24e9d9d0e3d14627eb54704260da10699c
Tony3Liu <118103359+tony3liu@users.noreply.github.com>
2025-02-10 16:58:44 +0800

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -1,7 +1,7 @@
 from uniai import deepseekChatLLM, zhipuChatLLM
 
 # chatLLM = zhipuChatLLM(model_name="GLM-4", api_key="93c8238ebf3fe2cf702cbc1ce5ec4d1b.xqkKBB72dleCYIjs")
-chatLLM = deepseekChatLLM(api_key='sk-39f0e28f0fa940089424d6ceccdeb585')
+chatLLM = deepseekChatLLM(api_key='')
 
 if __name__ == "__main__":
 
